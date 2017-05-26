2c6cd58a598d7f4b5ce63e1129e884e93615362e
Luke Anderson <stuntguy3000@gmail.com>
2017-05-26 10:49:48 +0930

    IR-3LED Solution

--- IR-3LED/IR-3LED.ino
+++ IR-3LED/IR-3LED.ino
@@ -2,7 +2,11 @@
 #include <IRremote.h>
 
 // Define our variables
-IR_PIN = 8;
+int IR_PIN = 8;
+
+int LED_1 = 5;
+int LED_2 = 6;
+int LED_3 = 7;
 
 // Setup the IR library
 IRrecv irDetect(IR_PIN);
@@ -11,22 +15,44 @@ decode_results irReciever;
 void setup() {
   // Start the IR reciever
   irDetect.enableIRIn();
+
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
 }
 
-void looop() {
+void loop() {
   // Check if we see an IR signal
   if (irDetect.decode(&irReciever)) {
     int lastValue = decodeIR();
-    disableLED()
 
-    irDetect-resume();
+    disableLEDs();
+    if (lastValue == 1) {
+      analogWrite(LED_1, 255);
+    } else if (lastValue == 2) {
+      analogWrite(LED_2, 255);
+    } else if (lastValue == 3) {
+      analogWrite(LED_3, 255);
+    } else {
+      for (int i = 0; i < 5; i++) {
+        analogWrite(LED_1, 255);
+        analogWrite(LED_2, 255);
+        analogWrite(LED_3, 255);
+        delay(200);
+        disableLEDs();
+        delay(200);
+      }
+    }
+    delay(1000);
+
+    irDetect.resume();
   }
 }
 
 void disableLEDs() {
   analogWrite(LED_1, 0);
-  analogWrite(LED_2, 0)
-  analogWrite(LED_, 0);
+  analogWrite(LED_2, 0);
+  analogWrite(LED_3, 0);
 }
 
 // Decode the IR Input to an integer value
